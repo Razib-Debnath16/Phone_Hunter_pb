b7503908c61848466dfef8e8a12be92d83b0e726
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-03-01 13:23:06 +0600

    adding sensors and others

--- JS2/phone.js
+++ JS2/phone.js
@@ -1,8 +1,10 @@
-//LoadPhones
+//Load Phones
 const loadPhones = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
+    searchField.textContent = '';
+    document.getElementById('phone-details').textContent = '';
     // console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
@@ -15,7 +17,7 @@ const displayPhones = phones => {
     // phoneSection.innerHTML = '';
     phoneSection.textContent = '';
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -36,7 +38,7 @@ const displayPhones = phones => {
     });
 };
 
-// loaddata by searching slug / phone_id
+// load data by searching slug / phone_id
 const loadphoneDetails = (phoneData) => {
     const url2 = `https://openapi.programming-hero.com/api/phone/${phoneData}`;
     // console.log(url2);
@@ -55,16 +57,27 @@ const displayPhoneDetails = details => {
     else {
         details.releaseDate = details.releaseDate;
     }
+
     div.innerHTML = `
     <div class="card w-100" style="background-color: rgba(255, 248, 220, 0.247);">
-                <img src="${details.image}" class="card-img-top" style="height: 250px; width: 250px;" alt="...">
+                <img src="${details.image}" class="card-img-top mx-auto" style="height: 250px; width: 250px;" alt="...">
                 <div class="card-body">
-                    <h2 class="card-text">Brand: ${details.brand}</h2>
-                    <h3 class="card-title">Model: ${details.name}</h3>
-                    <h4>Release Date : ${details.releaseDate}</h4>
-                    <h4>Chipset : ${details.mainFeatures.chipSet}</h4>
-                    <h4>Display Size: ${details.mainFeatures.displaySize}</h4>
-                    <h4>Storage : ${details.mainFeatures.memory}</h4>                  
+                    <h3 class="card-text"><strong>Brand :</strong> ${details.brand}</h3>
+                    <h3 class="card-title"><strong>Model :</strong> ${details.name}</h3>
+                    <h4><strong>Release Date :</strong> ${details.releaseDate}</h4>
+                    <h2 class="text-center"><strong>Features :</strong> </h2>
+                    <h4><strong>Chipset :</strong> ${details.mainFeatures.chipSet}</h4>
+                    <h4><strong>Display Size :</strong> ${details.mainFeatures.displaySize}</h4>
+                    <h4><strong>Storage :</strong> ${details.mainFeatures.memory}</h4>
+                    <h2 class="text-center"><strong>Sensors :</strong></h2>
+                    <h4>${details.mainFeatures.sensors}</h4>
+                    <h2 class="text-center"><strong>Others :</strong></h2>
+                    <h4><strong>Bluetooth : </strong>${details.others.Bluetooth}</h4>
+                    <h4><strong>GPS : </strong>${details.others.GPS}</h4>
+                    <h4><strong>NFC : </strong>${details.others.NFC}</h4>
+                    <h4><strong>Radio : </strong>${details.others.Radio}</h4>
+                    <h4><strong>USB : </strong>${details.others.USB}</h4>
+                    <h4><strong>WLAN : </strong>${details.others.WLAN}</h4>
                 </div>              
             </div>
     `
