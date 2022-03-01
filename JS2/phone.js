89dabc0845f4141453c2fd71edc94a6e29257f61
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-03-01 20:45:15 +0600

    adding spinner and validation

--- JS2/phone.js
+++ JS2/phone.js
@@ -1,30 +1,33 @@
-//Load Phones
-let count = 0;
 let phone1 = [];
 let phone2 = [];
+//Toggle Spinner
+const toggleSpinner = spin => {
+    document.getElementById('spinner').style.display = spin;
+}
+//Load Phones
 const loadPhones = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
     searchField.textContent = '';
     document.getElementById('phone-details').textContent = '';
-    // console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(response => response.json())
         .then(data => displayPhones(data.data));
 };
+
 // Show matching phones
 const displayPhones = phones => {
-    // console.log(phones);
+    toggleSpinner('block');
     const phoneSection = document.getElementById('phone-section');
-    // phoneSection.innerHTML = '';
     phoneSection.textContent = '';
-    // document.getElementById('search-field').value ==
     if (phones.length == 0) {
         const div = document.createElement('div');
+        document.getElementById('showMore-section').style.display = 'none';
         div.innerHTML = `
           <h1 class="text-center my-3">No Device Found </h1>
+          
         `;
         phoneSection.appendChild(div);
     }
@@ -37,9 +40,10 @@ const displayPhones = phones => {
                 phone1.push(phone)
             )
         });
-        console.log(phone1, phone2);
+        // console.log(phone1, phone2);
+
+        // show top 20 phones
         phone1.forEach(phone => {
-            // console.log(phone);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -59,19 +63,20 @@ const displayPhones = phones => {
             phone1 = [];
             phoneSection.appendChild(div);
         });
+        toggleSpinner('none');
         if (phones.length > 20) {
-            count = 0;
+
             document.getElementById('showMore-section').style.display = 'block';
         }
 
         document.getElementById('showMore-button').addEventListener('click', function () {
-            count++;
-            if (count == 1) {
-                phone2.forEach(phone => {
-                    // console.log(phone);
-                    const div = document.createElement('div');
-                    div.classList.add('col');
-                    div.innerHTML = `
+            // show remaining phones
+            phone2.forEach(phone => {
+
+                document.getElementById('phone-details').style.display = 'none';
+                const div = document.createElement('div');
+                div.classList.add('col');
+                div.innerHTML = `
              <div class="card w-100" style="background-color: rgba(255, 248, 220, 0.247);" >
                         <img src="${phone.image}" class="card-img-top" style="height: 250px; width: 250px;" alt="...">
                         <div class="card-body">
@@ -85,11 +90,10 @@ const displayPhones = phones => {
                     </div>
              
              `;
-                    phoneSection.appendChild(div);
-                    phone2 = [];
-                });
-                document.getElementById('showMore-section').style.display = 'none';
-            }
+                phoneSection.appendChild(div);
+                phone2 = [];
+            });
+            document.getElementById('showMore-section').style.display = 'none';
 
         })
 
@@ -106,17 +110,20 @@ const loadphoneDetails = (phoneData) => {
 };
 // display phone details
 const displayPhoneDetails = details => {
+    // console.log(details);
+    document.getElementById('phone-details').style.display = 'block';
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = '';
     const div = document.createElement('div');
     if (details.releaseDate == "") {
-        details.releaseDate = 'Not Available';
+        details.releaseDate = 'No Data Available';
     }
     else {
         details.releaseDate = details.releaseDate;
     }
-
-    div.innerHTML = `
+    // console.log(typeof details.others);
+    if (typeof details.others == 'object') {
+        div.innerHTML = `
     <div class="card w-100" style="background-color: rgba(255, 248, 220, 0.247);">
                 <img src="${details.image}" class="card-img-top mx-auto" style="height: 250px; width: 250px;" alt="...">
                 <div class="card-body">
@@ -137,13 +144,32 @@ const displayPhoneDetails = details => {
                     <h4><strong>USB : </strong>${details.others.USB}</h4>
                     <h4><strong>WLAN : </strong>${details.others.WLAN}</h4>
                 </div>              
-            </div>
-    `
+    </div>
+    `;
+    }
+    else {
+        div.innerHTML = `
+        <div class="card w-100" style="background-color: rgba(255, 248, 220, 0.247);">
+                    <img src="${details.image}" class="card-img-top mx-auto" style="height: 250px; width: 250px;" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-text"><strong>Brand :</strong> ${details.brand}</h3>
+                        <h3 class="card-title"><strong>Model :</strong> ${details.name}</h3>
+                        <h4><strong>Release Date :</strong> ${details.releaseDate}</h4>
+                        <h2 class="text-center"><strong>Features :</strong> </h2>
+                        <h4><strong>Chipset :</strong> ${details.mainFeatures.chipSet}</h4>
+                        <h4><strong>Display Size :</strong> ${details.mainFeatures.displaySize}</h4>
+                        <h4><strong>Storage :</strong> ${details.mainFeatures.memory}</h4>
+                        <h2 class="text-center"><strong>Sensors :</strong></h2>
+                        <h4>${details.mainFeatures.sensors}</h4>
+                    </div>              
+        </div>
+        `;
+    }
     phoneDetails.appendChild(div);
 }
 
 
-// 
+// eventlistener on search button
 document.getElementById('search-button').addEventListener('click', function () {
 
     loadPhones();
